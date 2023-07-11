e5578c799e4d8c52a64ce2bc6f54993457c61fa8
Alexander Colloredo-Mansfeld <acollore@ucsc.edu>
2023-07-10 20:03:35 -0400

    database definitions

--- models.py
+++ models.py
@@ -5,11 +5,27 @@ This file defines the database models
 from .common import db, Field
 from pydal.validators import *
 
-### Define your table below
+# Define your table below
 #
 # db.define_table('thing', Field('name'))
 #
-## always commit your models to avoid problems later
-#
-# db.commit()
-#
+# always commit your models to avoid problems later
+
+
+db.define_table(
+    'gallery',
+    Field("owner", "integer", "reference auth_user"),
+    Field("collection_items", "list:reference collection_item")
+)
+
+db.define_table(
+    'collection_item',
+    Field("owner", "integer", "reference auth_user"),
+    Field("artist"),
+    Field("type"),
+    Field("creation_date", "date"),
+    Field("origin"),
+    Field("gallery", "integer", "reference gallery")
+)
+
+db.commit()
